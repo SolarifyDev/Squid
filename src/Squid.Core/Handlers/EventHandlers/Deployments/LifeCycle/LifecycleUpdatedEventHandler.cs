297f744e62a00833d52771dc05db7f9106d862fc
zirui liu <maxon.liu@sjfood.com>
2025-07-16 19:04:56 +0800

    rename

--- src/Squid.Core/Handlers/EventHandlers/Deployments/LifeCycle/LifecycleUpdatedEventHandler.cs
+++ src/Squid.Core/Handlers/EventHandlers/Deployments/LifeCycle/LifecycleUpdatedEventHandler.cs
@@ -2,7 +2,7 @@ using Squid.Message.Events.Deployments.LifeCycle;
 
 namespace Squid.Core.Handlers.EventHandlers.Deployments.LifeCycle;
 
-public class UpdateLifeCycleEventHandler : IEventHandler<LifeCycleUpdatedEvent>
+public class LifecycleUpdatedEventHandler : IEventHandler<LifeCycleUpdatedEvent>
 {
     public async Task Handle(IReceiveContext<LifeCycleUpdatedEvent> context, CancellationToken cancellationToken)
     {
