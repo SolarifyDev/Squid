db3e509eded5ca530ead5273d005845079117474
TATAN.H <120452548+doudouta@users.noreply.github.com>
2025-11-28 14:24:03 +0800

    update

--- src/Squid.Core/Services/Deployments/Project/ProjectDataProvider.cs
+++ src/Squid.Core/Services/Deployments/Project/ProjectDataProvider.cs
@@ -12,7 +12,8 @@ public interface IProjectDataProvider : IScopedDependency
     
     Task CreateProjectGroup(ProjectGroup projectGroup, bool forceSave = false, CancellationToken cancellationToken = default);
 
-    Task<List<(ProjectGroup, List<Message.Domain.Deployments.Project>)>> GetProjectGroupWithProjectsPagingAsync(int spaceId, int? projectGroupId = null, int? projectId = null, int? environmentId = null, int? pageIndex = null, int? pageSize = null, CancellationToken cancellationToken = default);
+    Task<List<(ProjectGroup, List<Message.Domain.Deployments.Project>)>> GetProjectGroupWithProjectsPagingAsync(int spaceId, int? projectGroupId = null, int? projectId = null,
+        int? environmentId = null, string keyWord = null, int? pageIndex = null, int? pageSize = null, CancellationToken cancellationToken = default);
 }
 
 public class ProjectDataProvider : IProjectDataProvider
@@ -76,9 +77,59 @@ public class ProjectDataProvider : IProjectDataProvider
             await _unitOfWork.SaveChangesAsync(cancellationToken).ConfigureAwait(false);
     }
 
-    public Task<List<(ProjectGroup, List<Message.Domain.Deployments.Project>)>> GetProjectGroupWithProjectsPagingAsync(int spaceId, int? projectGroupId = null, int? projectId = null,
-        int? environmentId = null, int? pageIndex = null, int? pageSize = null, CancellationToken cancellationToken = default)
+    public async Task<List<(ProjectGroup, List<Message.Domain.Deployments.Project>)>> GetProjectGroupWithProjectsPagingAsync(int spaceId, int? projectGroupId = null, int? projectId = null,
+        int? environmentId = null, string keyWord = null, int? pageIndex = null, int? pageSize = null, CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
+        var groupQuery = _repository.Query<ProjectGroup>().Where(g => g.SpaceId == spaceId);
+
+        if (projectGroupId.HasValue)
+            groupQuery = groupQuery.Where(g => g.Id == projectGroupId.Value);
+
+        var projectQuery = _repository.Query<Message.Domain.Deployments.Project>()
+            .Where(p => p.SpaceId == spaceId);
+
+        if (projectGroupId.HasValue)
+            projectQuery = projectQuery.Where(p => p.ProjectGroupId == projectGroupId.Value);
+
+        if (projectId.HasValue)
+            projectQuery = projectQuery.Where(p => p.Id == projectId.Value);
+
+        if (!string.IsNullOrWhiteSpace(keyWord))
+        {
+            keyWord = keyWord.Trim();
+            groupQuery = groupQuery.Where(g => g.Name.Contains(keyWord));
+            projectQuery = projectQuery.Where(p => p.Name.Contains(keyWord));
+        }
+
+        if (pageIndex.HasValue && pageSize.HasValue &&
+            pageIndex.Value >= 1 && pageSize.Value > 0)
+        {
+            int skip = (pageIndex.Value - 1) * pageSize.Value;
+            groupQuery = groupQuery.Skip(skip).Take(pageSize.Value);
+        }
+
+        var groups = await groupQuery.ToListAsync(cancellationToken).ConfigureAwait(false);
+
+        if (!groups.Any())
+            return new List<(ProjectGroup, List<Message.Domain.Deployments.Project>)>();
+
+        var groupIds = groups.Select(g => g.Id).ToList();
+
+        var projects = await projectQuery
+            .Where(p => groupIds.Contains(p.ProjectGroupId))
+            .ToListAsync(cancellationToken)
+            .ConfigureAwait(false);
+
+        var result = groups
+            .Select(g =>
+            {
+                var relatedProjects = projects
+                    .Where(p => p.ProjectGroupId == g.Id)
+                    .ToList();
+
+                return (g, relatedProjects);
+            }).ToList();
+
+        return result;
     }
 }
