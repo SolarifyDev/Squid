c17ddc2c884cdfc988d0d29c7ff1a81b1a529734
zirui liu <maxon.liu@sjfood.com>
2025-12-09 10:21:13 +0800

    add project

---
+++ src/Squid.Message/Events/Deployments/Project/ProjectCreatedEvent.cs
@@ -0,0 +1,9 @@
+using Squid.Message.Models.Deployments.Project;
+
+namespace Squid.Message.Events.Deployments.Project;
+
+public class ProjectCreatedEvent : IEvent
+{
+    public ProjectDto Data { get; set; }
+}
+
