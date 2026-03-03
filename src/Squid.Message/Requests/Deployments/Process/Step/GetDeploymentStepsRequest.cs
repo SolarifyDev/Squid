4625bf5d6627ed21c49427cecfbd426b11241f56
Mars.P <marsxxd@outlook.com>
2026-03-03 14:43:09 +0800

    fix page index

--- src/Squid.Message/Requests/Deployments/Process/Step/GetDeploymentStepsRequest.cs
+++ src/Squid.Message/Requests/Deployments/Process/Step/GetDeploymentStepsRequest.cs
@@ -7,9 +7,9 @@ public class GetDeploymentStepsRequest : IPaginatedRequest
 {
     public int ProcessId { get; set; }
 
-    public int PageIndex { get; set; }
+    public int PageIndex { get; set; } = 1;
 
-    public int PageSize { get; set; }
+    public int PageSize { get; set; } = 20;
 
     public string Keyword { get; set; }
 }
