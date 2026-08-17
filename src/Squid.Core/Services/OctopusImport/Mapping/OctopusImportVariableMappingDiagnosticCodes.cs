be1ec4b7997611ae47602df921c3bb29e97d5a02
Mindy.L <mindy.li@sjfood.com>
2026-08-17 11:08:46 +0800

    Variable-set mapper for Octopus import

---
+++ src/Squid.Core/Services/OctopusImport/Mapping/OctopusImportVariableMappingDiagnosticCodes.cs
@@ -0,0 +1,13 @@
+namespace Squid.Core.Services.OctopusImport.Mapping;
+
+public static class OctopusImportVariableMappingDiagnosticCodes
+{
+    public const string MissingProjectMapping = "OctopusImport.Variable.MissingProjectMapping";
+    public const string UnsupportedVariableSetOwnerType = "OctopusImport.Variable.UnsupportedVariableSetOwnerType";
+    public const string SensitiveValueOmitted = "OctopusImport.Variable.SensitiveValueOmitted";
+    public const string UnsupportedVariableType = "OctopusImport.Variable.UnsupportedVariableType";
+    public const string PromptDisplaySettingsOmitted = "OctopusImport.Variable.PromptDisplaySettingsOmitted";
+    public const string MissingScopeMapping = "OctopusImport.Variable.MissingScopeMapping";
+    public const string UnsupportedScopeType = "OctopusImport.Variable.UnsupportedScopeType";
+    public const string EmptyScopeValue = "OctopusImport.Variable.EmptyScopeValue";
+}
